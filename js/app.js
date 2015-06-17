1380f7a3897c58d4f85465f33ea52ecf3fcc723d
Shawn Hanna <shawn524@gmail.com>
2015-06-17 11:55:30 -0400

    Added 'nothing' with 0.4 chance and also a reset game function

--- js/app.js
+++ js/app.js
@@ -17,83 +17,87 @@ var playerAttributes = {
 	'gameOver': false
 }
 var food = {
-	name:'food',
-	total:0,
-	increment:1,
-	chance:0.2
+	name: 'food',
+	total: 0,
+	increment: 1,
+	chance: 0.1
 };
 var water = {
-	name:'water',
-	total:0,
-	increment:1,
-	chance:0.3
+	name: 'water',
+	total: 0,
+	increment: 1,
+	chance: 0.2
 };
 var ammo = {
-	name:'ammo',
-	total:0,
-	increment:1,
-	chance:0.2
+	name: 'ammo',
+	total: 0,
+	increment: 1,
+	chance: 0.1
 };
 var scrap = {
-	name:'scrap',
-	total:0,
-	increment:1,
-	chance:0.2
+	name: 'scrap',
+	total: 0,
+	increment: 1,
+	chance: 0.2
 };
 var medpack = {
-	name:'medpack',
-	total:0,
-	increment:1,
-	chance:0.1
+	name: 'medpack',
+	total: 0,
+	increment: 1,
+	chance: 0.1
+};
+var nothing = {
+	name: 'nothing',
+	chance: 0.4,
 };
 
-var basicSupplies = [food, water, ammo, scrap, medpack];
-var weight = [food.chance, water.chance, ammo.chance, scrap.chance, medpack.chance];
+var basicSupplies = [food, water, ammo, scrap, medpack, nothing];
+var weight = [food.chance, water.chance, ammo.chance, scrap.chance, medpack.chance, nothing.chance];
 var lesserMulti = 1;
 var greaterMulti = 1;
 var daysSurvived = 0;
 
 // Generates a random number between min and max.
 var rand = function(min, max) {
-    return Math.random() * (max - min) + min;
+	return Math.random() * (max - min) + min;
 };
- 
+
 // This function is the main click action. 
 // It can take all of the basic supplies you might find and return
 // a single random-weighted result as a loot drop. 
 var gatherSupplies = function(basicSupplies, weight) {
-    var total_weight = weight.reduce(function (prev, cur, i, arr) {
-        return prev + cur;
-    });
-    // console.log('total weight',total_weight);
-     
-    var random_num = rand(0, total_weight);
-    // console.log('random num', random_num);
-    var weight_sum = 0;
-     
-    for (var i = 0; i < basicSupplies.length; i++) {
-        weight_sum += weight[i];
-        // console.log(weight[i]);
-        weight_sum = +weight_sum.toFixed(2);
-        // console.log('weight sum',weight_sum);
-        var item_drop = basicSupplies[i];
-         
-        if (random_num <= weight_sum) {
-        	item_drop.total++;
-            console.log('found some',basicSupplies[i].name);
-            gameLog('You found some ' + basicSupplies[i].name);
-            break;
-        }
-    }
+	var total_weight = weight.reduce(function(prev, cur, i, arr) {
+		return prev + cur;
+	});
+	// console.log('total weight',total_weight);
 
-    // 10 clicks to a day
-    daysSurvived += 0.1;
-    // Apply certain functions when button pressed
-    updateTotals();
-    applyStatusEffect();
-    playerStatsRemoval();
-    deathCheck();
-	
+	var random_num = rand(0, total_weight);
+	// console.log('random num', random_num);
+	var weight_sum = 0;
+
+	for (var i = 0; i < basicSupplies.length; i++) {
+		weight_sum += weight[i];
+		// console.log(weight[i]);
+		weight_sum = +weight_sum.toFixed(2);
+		// console.log('weight sum',weight_sum);
+		var item_drop = basicSupplies[i];
+
+		if (random_num <= weight_sum) {
+			item_drop.total++;
+			console.log('found some', basicSupplies[i].name);
+			gameLog('You found some ' + basicSupplies[i].name);
+			break;
+		}
+	}
+
+	// 10 clicks to a day
+	daysSurvived += 0.1;
+	// Apply certain functions when button pressed to progress game
+	updateTotals();
+	applyStatusEffect();
+	playerStatsRemoval();
+	deathCheck();
+
 };
 
 function playerStatsRemoval() {
@@ -101,31 +105,54 @@ function playerStatsRemoval() {
 	playerAttributes.thirst += (1 * lesserMulti * greaterMulti);
 	playerAttributes.rads += (1 * lesserMulti * greaterMulti);
 	// Health removal
-	if(playerAttributes.hungry || playerAttributes.thirsty || playerAttributes.sick) {
+	if (playerAttributes.hungry || playerAttributes.thirsty || playerAttributes.sick) {
 		playerAttributes.health -= (1 * lesserMulti * greaterMulti);
 	}
-	if(playerAttributes.starving || playerAttributes.dehydrated) {
-		playerAttributes.health -= (1 * lesserMulti * greaterMulti);	
+	if (playerAttributes.starving || playerAttributes.dehydrated) {
+		playerAttributes.health -= (1 * lesserMulti * greaterMulti);
 	}
 }
 
 function deathCheck() {
-	if(playerAttributes.health == 0 && !playerAttributes.gameOver) {
+	if (playerAttributes.health == 0 && !playerAttributes.gameOver) {
 		alert("You have succumbed to deaths cold embrace. Your journey has ended.")
 		playerAttributes.gameOver = true;
-		// reset game
+		resetGame();
+	}
+}
+
+function resetGame() {
+	if (confirm("Reset game?")) {
+		playerAttributes.health = 100;
+		playerAttributes.hunger = 0;
+		playerAttributes.hungry = false;
+		playerAttributes.starving = false;
+		playerAttributes.thirst = 0;
+		playerAttributes.thirsty = false;
+		playerAttributes.dehydrated = false;
+		playerAttributes.rads = 10;
+		playerAttributes.sick = false;
+		playerAttributes.gameOver = false;
+		food.total = 0;
+		water.total = 0;
+		ammo.total = 0;
+		scrap.total = 0;
+		medpack.total = 0;
+		daysSurvived = 0;
+	} else {
+		// disable buttons?
 	}
 }
 
 function updateTotals() {
 	// Supplies
-    document.getElementById('food_count').innerHTML = food.total.toFixed(0);
+	document.getElementById('food_count').innerHTML = food.total.toFixed(0);
 	document.getElementById('water_count').innerHTML = water.total.toFixed(0);
 	document.getElementById('ammo_count').innerHTML = ammo.total.toFixed(0);
 	document.getElementById('scrap_metal_count').innerHTML = scrap.total.toFixed(0);
 	document.getElementById('medical_supplies_count').innerHTML = medpack.total.toFixed(0);
 	// Attributes
-	document.getElementById('health_count').innerHTML = playerAttributes.health.toFixed(0);	
+	document.getElementById('health_count').innerHTML = playerAttributes.health.toFixed(0);
 	document.getElementById('hunger_count').innerHTML = playerAttributes.hunger.toFixed(0);
 	document.getElementById('thirst_count').innerHTML = playerAttributes.thirst.toFixed(0);
 	document.getElementById('rads_count').innerHTML = playerAttributes.rads.toFixed(0);
@@ -136,64 +163,64 @@ function updateTotals() {
 
 function statsLimiter() {
 	// Player stats limiter
-	if(playerAttributes.health >= 100) {
+	if (playerAttributes.health >= 100) {
 		playerAttributes.health = 100;
 	}
-	if(playerAttributes.hunger >= 100) {
+	if (playerAttributes.hunger >= 100) {
 		playerAttributes.hunger = 100;
 	}
-	if(playerAttributes.thirst >= 100) {
+	if (playerAttributes.thirst >= 100) {
 		playerAttributes.thirst = 100;
 	}
-	if(playerAttributes.rads >= 200) {
+	if (playerAttributes.rads >= 200) {
 		playerAttributes.rads = 200;
 	}
-	if(playerAttributes.health <= 0) {
+	if (playerAttributes.health <= 0) {
 		playerAttributes.health = 0;
 	}
-	if(playerAttributes.hunger <= 0) {
+	if (playerAttributes.hunger <= 0) {
 		playerAttributes.hunger = 0;
 	}
-	if(playerAttributes.thirst <= 0) {
+	if (playerAttributes.thirst <= 0) {
 		playerAttributes.thirst = 0;
 	}
-	if(playerAttributes.rads <= 0) {
+	if (playerAttributes.rads <= 0) {
 		playerAttributes.rads = 0;
 	}
 }
 
 function applyStatusEffect() {
-// Hunger
-	if(playerAttributes.hunger < 60) {
+	// Hunger
+	if (playerAttributes.hunger < 60) {
 		playerAttributes.hungry = false;
 		playerAttributes.starving = false;
-	} else if((playerAttributes.hunger >= 60 && playerAttributes.hunger < 100) && !playerAttributes.starving) {
+	} else if ((playerAttributes.hunger >= 60 && playerAttributes.hunger < 100) && !playerAttributes.starving) {
 		playerAttributes.hungry = true;
 		gameLog("You are hungry");
-	} else if(playerAttributes.hunger >= 100) {
+	} else if (playerAttributes.hunger >= 100) {
 		playerAttributes.hungry = false;
 		playerAttributes.starving = true;
 		gameLog("You are starving");
 	};
 	// Thirst
-	if(playerAttributes.thirst < 60) {
+	if (playerAttributes.thirst < 60) {
 		playerAttributes.thirsty = false;
 		playerAttributes.dehydrated = false;
-	} else if((playerAttributes.thirst >= 60 && playerAttributes.thirst < 100) && !playerAttributes.dehydrated) {
+	} else if ((playerAttributes.thirst >= 60 && playerAttributes.thirst < 100) && !playerAttributes.dehydrated) {
 		playerAttributes.thirsty = true;
 		gameLog("You are thirsty");
-	} else if(playerAttributes.thirst >= 100) {
+	} else if (playerAttributes.thirst >= 100) {
 		playerAttributes.thirsty = false;
 		playerAttributes.dehydrated = true;
 		gameLog("You are dehydrated");
 	};
 	// Rad sickness
-	if(playerAttributes.rads < 100) {
+	if (playerAttributes.rads < 100) {
 		playerAttributes.sick = false;
-	} else if(playerAttributes.rads >= 100 && playerAttributes.rads < 200){
+	} else if (playerAttributes.rads >= 100 && playerAttributes.rads < 200) {
 		playerAttributes.sick = true;
 		gameLog("You have radiation sickness");
-	} else if(playerAttributes.rads == 200) {
+	} else if (playerAttributes.rads == 200) {
 		playerAttributes.health = 0;
 	};
 };
@@ -203,44 +230,44 @@ function statusMulti() {
 	// Status multiplier
 	// Lesser status effects
 	// If none or one 
-	if(!playerAttributes.hungry && !playerAttributes.thirsty && !playerAttributes.sick) {
+	if (!playerAttributes.hungry && !playerAttributes.thirsty && !playerAttributes.sick) {
 		lesserMulti = 1.0;
 	}
-	if(playerAttributes.hungry || playerAttributes.thirsty || playerAttributes.sick) {
+	if (playerAttributes.hungry || playerAttributes.thirsty || playerAttributes.sick) {
 		lesserMulti = 1.5;
 	};
 	// If any two
-	if(playerAttributes.hungry && playerAttributes.thirsty) {
+	if (playerAttributes.hungry && playerAttributes.thirsty) {
 		lesserMulti = 3.0;
-	} else if(playerAttributes.hungry && playerAttributes.sick){
+	} else if (playerAttributes.hungry && playerAttributes.sick) {
 		lesserMulti = 3.0;
-	} else if(playerAttributes.thirsty && playerAttributes.sick){
+	} else if (playerAttributes.thirsty && playerAttributes.sick) {
 		lesserMulti = 3.0;
 	};
 	// If three
-	if(playerAttributes.hungry && playerAttributes.thirsty && playerAttributes.sick) {
+	if (playerAttributes.hungry && playerAttributes.thirsty && playerAttributes.sick) {
 		lesserMulti = 4.5;
 	};
 
 	// Greater status effects
 	// If one or none
-	if(!playerAttributes.starving && !playerAttributes.dehydrated) {
+	if (!playerAttributes.starving && !playerAttributes.dehydrated) {
 		greaterMulti = 1.0;
 	}
-	if(playerAttributes.starving || playerAttributes.dehydrated) {
+	if (playerAttributes.starving || playerAttributes.dehydrated) {
 		greaterMulti = 2.0;
 	}
 	// If both
-	if(playerAttributes.starving && playerAttributes.dehydrated) {
+	if (playerAttributes.starving && playerAttributes.dehydrated) {
 		greaterMulti = 4.0;
 	}
 
 	// Display to player
 	// Hunger
-	if(playerAttributes.hungry) {
+	if (playerAttributes.hungry) {
 		document.getElementById('hungry_status').className = "hungry_status";
 		document.getElementById('starving_status').className += " hidden";
-	} else if(playerAttributes.starving) {
+	} else if (playerAttributes.starving) {
 		document.getElementById('starving_status').className = "starving_status";
 		document.getElementById('hungry_status').className += " hidden";
 	} else {
@@ -248,10 +275,10 @@ function statusMulti() {
 		document.getElementById('starving_status').className += " hidden";
 	};
 	// Thirst
-	if(playerAttributes.thirsty) {
+	if (playerAttributes.thirsty) {
 		document.getElementById('thirsty_status').className = "thirsty_status";
 		document.getElementById('dehydrated_status').className += " hidden";
-	} else if(playerAttributes.dehydrated) {
+	} else if (playerAttributes.dehydrated) {
 		document.getElementById('dehydrated_status').className = "dehydrated_status";
 		document.getElementById('thirsty_status').className += " hidden";
 	} else {
@@ -259,7 +286,7 @@ function statusMulti() {
 		document.getElementById('dehydrated_status').className += " hidden";
 	};
 	// Rad sickness
-	if(playerAttributes.sick) {
+	if (playerAttributes.sick) {
 		document.getElementById('sick_status').className = "sick_status";
 	} else {
 		document.getElementById('sick_status').className += " hidden";
@@ -268,32 +295,33 @@ function statusMulti() {
 
 // Item usage
 function useItem(item) {
-	if(item.total > 0) {
+	if (item.total > 0) {
 		item.total -= 1;
-		if(item.name == 'medpack') {
+		if (item.name == 'medpack') {
 			playerAttributes.health += 20;
 			playerAttributes.rads -= 20;
-		} else if(item.name == 'food') {
+		} else if (item.name == 'food') {
 			playerAttributes.hunger -= 10;
-		} else if(item.name == 'water') {
+		} else if (item.name == 'water') {
 			playerAttributes.thirst -= 12;
 		}
 	} else {
-		console.log('Not enough', item.name);
+		gameLog('Not enough ' + item.name);
+		// console.log('Not enough', item.name);
 	}
 }
 
 // In-game log message
-function gameLog(message){
+function gameLog(message) {
 	// Check to see if the last message was the same as this one, if so just increment the (xNumber) value
-	if (document.getElementById('logL').innerHTML == message){
+	if (document.getElementById('logL').innerHTML == message) {
 		logRepeat += 1;
 		document.getElementById('log0').innerHTML = '<td id="logT">' + '</td><td id="logL">' + message + '</td><td id="logR">(x' + logRepeat + ')</td>';
 	} else {
 		// Reset the (xNumber) value
 		logRepeat = 1
-		// Go through all the logs in order, moving them down one and successively overwriting them.
-		// Bottom five elements temporarily removed, may be readded later.
+			// Go through all the logs in order, moving them down one and successively overwriting them.
+			// Bottom five elements temporarily removed, may be readded later.
 		document.getElementById('log9').innerHTML = document.getElementById('log8').innerHTML
 		document.getElementById('log8').innerHTML = document.getElementById('log7').innerHTML
 		document.getElementById('log7').innerHTML = document.getElementById('log6').innerHTML
@@ -302,7 +330,7 @@ function gameLog(message){
 		document.getElementById('log4').innerHTML = document.getElementById('log3').innerHTML
 		document.getElementById('log3').innerHTML = document.getElementById('log2').innerHTML
 		document.getElementById('log2').innerHTML = document.getElementById('log1').innerHTML
-		// Since ids need to be unique, log1 strips the ids from the log0 elements when copying the contents.
+			// Since ids need to be unique, log1 strips the ids from the log0 elements when copying the contents.
 		document.getElementById('log1').innerHTML = '<td>' + document.getElementById('logT').innerHTML + '</td><td>' + document.getElementById('logL').innerHTML + '</td><td>' + document.getElementById('logR').innerHTML + '</td>';
 		// Creates new contents with new message, and x1
 		document.getElementById('log0').innerHTML = '<td id="logT">' + '</td><td id="logL">' + message + '</td><td id="logR">(x' + logRepeat + ')</td>';
@@ -310,11 +338,11 @@ function gameLog(message){
 }
 
 // Main timer function
-window.setInterval(function(){
+window.setInterval(function() {
 	statsLimiter();
 	statusMulti();
 	updateTotals();
 	applyStatusEffect();
 	deathCheck();
-	console.log('multi ' + (lesserMulti * greaterMulti))
+	// console.log('multi ' + (lesserMulti * greaterMulti))
 }, 1000);
